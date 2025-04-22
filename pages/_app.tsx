0cce2159be097de2cbb365fefeec958b54622594
Andreprataama <andreprataama07@gmail.com>
2025-04-22 23:34:50 +0700

    tugas hari-1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
+import RootLayout from "@/layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const router = useRouter()
+  const MetaTitle = router.pathname === '/' ? 'Home' : router.pathname.replace ('/','')
+
+  return (
+    <RootLayout MetaTitle={MetaTitle}>
+      <Component {...pageProps} />
+    </RootLayout>
+  )
 }
